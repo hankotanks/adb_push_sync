45f2bde94cf54b9db3a01dfe5d062bb9bf6efb68
Hank Lewis <hank@modernatomic.com>
2026-02-05 22:00:38 +0100

    patch scope is no longer fixed

--- adb_push_sync.py
+++ adb_push_sync.py
@@ -3,6 +3,9 @@ import os
 import sys
 import subprocess
 import datetime
+import tqdm
+import pathlib
+import re
 
 class DeviceShell:
     def __init__(self):
@@ -20,51 +23,56 @@ class DeviceShell:
             text = True,
             bufsize = 1)
 
-        self.run("export PS1='__PROMPT__'")
-
-    def run(self, cmd):
-        SENTINEL = "__ADB_PUSH_SYNC_DONE__"
-        SENTINEL_CMD = f"echo {SENTINEL}"
-
-        cmds = [
-            cmd,
-            "echo $?",
-            SENTINEL_CMD
-        ]
-
-        for cmd_i in cmds: self.proc.stdin.write(cmd_i + "\n")
+        self.proc.stdin.write("export PS1='__PROMPT__'" + "\n")
         self.proc.stdin.flush()
 
-        out = []
+    def run(self, cmd):
+        SENTINEL_BEGIN = "__ADB_PUSH_SYNC_BEGIN__"
+        SENTINEL_END = "__ADB_PUSH_SYNC_END__"
 
+        for cmd_i in [f"echo {SENTINEL_BEGIN}", cmd, "echo $?", f"echo {SENTINEL_END}"]: 
+            self.proc.stdin.write(cmd_i + "\n")
+        self.proc.stdin.flush()
+
+        output = []
+
+        started = False
         for idx, line in enumerate(self.proc.stdout):
             line = line.rstrip("\n")
-            if line == SENTINEL:
-                break
-            if len(line) == 0 or line.startswith("__PROMPT__") or line == SENTINEL_CMD: continue
-            out.append(line)
-
+            if line == SENTINEL_BEGIN: 
+                started = True
+                continue
+            if not started: continue
+            if line == SENTINEL_END: break
+            if len(line) == 0 or line.startswith("__PROMPT__") or "\x08" in line: continue
+            output.append(line)
+        
         try:
-            exit_code = int(out[-1])
+            exit_code = int(output[-1])
         except:
             raise RuntimeError("Unreachable!")
 
-        return exit_code, out[:-1]
+        return exit_code, output[0:-1]
 
     def exists(self, path):
         exit_code, _ = self.run(f"[ -e \"{path}\" ]")
         return True if exit_code == 0 else False
 
-    def get_mod_stamp(self, path):
-        exit_code, result = shell.run(f"ls -l \"{path_dst}\"")
+    def getmtime(self, path):
+        exit_code, result = shell.run(f"ls -l \"{path}\"")
         if exit_code != 0 or len(result) < 2: 
             return None
 
         cols = result[-1].split()
-        if len(cols) < 7: 
+        if len(cols) == 0:
             return None
 
-        return datetime.datetime.strptime(" ".join(cols[4:6]), "%Y-%m-%d %H:%M").timestamp()
+        is_file = cols[0][0] == '-'
+        if len(cols) < (6 if is_file else 5): 
+            return None
+
+        time_str = " ".join(cols[4:6] if is_file else cols[3:5])
+        return datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M").timestamp()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -87,31 +95,43 @@ if __name__ == "__main__":
     args.source = args.source.rstrip("/")
     args.destination = os.path.join(args.destination, os.path.basename(args.source))
 
-    files = [os.path.join(dirname, fname).removeprefix(args.source).lstrip("/") 
-        for dirname, _, fnames in os.walk(args.source) for fname in fnames]
+    dirs = [(dirname.removeprefix(args.source).lstrip("/"), fnames) 
+        for dirname, _, fnames in os.walk(args.source) if fnames]
 
-    files_to_push = []
+    dirs_to_push = []
+    for entry, fnames in tqdm.tqdm(dirs, desc = "Indexing"):
+        path_src = os.path.join(args.source, entry)
+        path_dst = os.path.join(args.destination, entry)
 
-    for file in files:
-        path_src = os.path.join(args.source, file)
-        path_dst = os.path.join(args.destination, file)
-
-        push_file = False
         if shell.exists(path_dst):
-            mod_src = os.path.getmtime(path_src)
-            mod_dst = shell.get_mod_stamp(path_dst)
-            if mod_dst is None:
-                raise RuntimeError(f"Failed to parse file: {path_dst}")
-
-            push_file = mod_dst < mod_src
+            path_dst_mod = shell.getmtime(path_dst)
+            exit_code, fnames_dst = shell.run(f"ls -l \"{path_dst}\"")
+            if exit_code != 0:
+                raise RuntimeError("Unreachable!")
+            if len(fnames) == len(fnames_dst) and path_dst_mod is not None and path_dst_mod >= os.path.getmtime(path_src): continue
+            dirs_to_push.append((entry, True))
         else:
-            push_file = True
+            dirs_to_push.append((entry, False))
 
-        if push_file:
-            files_to_push.append((path_src, path_dst))
+    dirs_to_push_filtered = []
+    class BreakOuter(Exception): pass
+    for entry_a, exists_a in dirs_to_push:
+        try:
+            for entry_b, exists_b in dirs_to_push:
+                if entry_a == entry_b: continue
+                if pathlib.Path(entry_a).resolve().is_relative_to(pathlib.Path(entry_b).resolve()):
+                    raise BreakOuter;
+            dirs_to_push_filtered.append((entry_a, exists_a))
+        except BreakOuter:
+            pass
+    
+    for entry, exists in tqdm.tqdm(dirs_to_push_filtered, desc = "Patching"):
+        path_src = os.path.join(args.source, entry)
+        path_dst = os.path.join(args.destination, entry)
+        if exists: 
+            path_dst = os.path.dirname(path_dst)
 
-    for path_src, path_dst in files_to_push:
-        cmd = subprocess.run(["adb", "push", path_src, path_dst],
-            stdout = sys.stdout, stderr = sys.stderr)
+        cmd = subprocess.run(["adb", "push", f"{path_src}", f"{path_dst}"],
+            stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
         if cmd.returncode != 0:
-            raise RuntimeError(f"Failed to copy file: {path_src}")
+            raise RuntimeError(f"Failed to copy file: {entry}")
