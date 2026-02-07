85f4a6895659928e506d5f8e065320350600bfd8
Hank Lewis <hank@modernatomic.com>
2026-02-07 08:56:43 +0100

    changed diff behavior

--- adb_push_sync.py
+++ adb_push_sync.py
@@ -7,72 +7,73 @@ import tqdm
 import pathlib
 import re
 
-class DeviceShell:
-    def __init__(self):
-        adb_check = subprocess.run(["which", "adb"],
-            stdout = subprocess.DEVNULL,
-            stderr = subprocess.DEVNULL)
+def run_on_device(cmd):
+    result = subprocess.run(["adb", "shell", *cmd, "&&", "echo", "$?"], 
+        capture_output = True, text = True)
 
-        if adb_check.returncode != 0:
-            raise RuntimeError("Unable to find adb in PATH!")
-        
-        self.proc = subprocess.Popen(["adb", "shell"],
-            stdin = subprocess.PIPE,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.STDOUT,
-            text = True,
-            bufsize = 1)
+    output = result.stdout.strip().split("\n")
+    if len(output) == 0:
+        raise RuntimeError("Unreachable!")
 
-        self.proc.stdin.write("export PS1='__PROMPT__'" + "\n")
-        self.proc.stdin.flush()
+    try:
+        exit_code = int(output[-1])
+    except ValueError as e:
+        exit_code = 1
 
-    def run(self, cmd):
-        SENTINEL_BEGIN = "__ADB_PUSH_SYNC_BEGIN__"
-        SENTINEL_END = "__ADB_PUSH_SYNC_END__"
+    return exit_code, output[:-1]
 
-        for cmd_i in [f"echo {SENTINEL_BEGIN}", cmd, "echo $?", f"echo {SENTINEL_END}"]: 
-            self.proc.stdin.write(cmd_i + "\n")
-        self.proc.stdin.flush()
+def exists_on_device(path):
+    exit_code, _ = run_on_device([f"[ -e \"{path}\" ]"])
+    return True if exit_code == 0 else False
 
-        output = []
+"""
+def getmtime_on_device(path):
+    exit_code, result = run_on_device(["ls", "-l",  f"{path}"])
+    print(result)
+    return None
+    if exit_code != 0 or len(result) < 2: 
+        return None
 
-        started = False
-        for idx, line in enumerate(self.proc.stdout):
-            line = line.rstrip("\n")
-            if line == SENTINEL_BEGIN: 
-                started = True
-                continue
-            if not started: continue
-            if line == SENTINEL_END: break
-            if len(line) == 0 or line.startswith("__PROMPT__") or "\x08" in line: continue
-            output.append(line)
-        
-        try:
-            exit_code = int(output[-1])
-        except:
-            raise RuntimeError("Unreachable!")
+    cols = result[-1].split()
+    if len(cols) == 0:
+        return None
 
-        return exit_code, output[0:-1]
+    is_file = cols[0][0] == '-'
+    if len(cols) < (6 if is_file else 5): 
+        return None
 
-    def exists(self, path):
-        exit_code, _ = self.run(f"[ -e \"{path}\" ]")
-        return True if exit_code == 0 else False
+    time_str = " ".join(cols[4:6] if is_file else cols[3:5])
+    return datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M").timestamp()
+"""
 
-    def getmtime(self, path):
-        exit_code, result = shell.run(f"ls -l \"{path}\"")
-        if exit_code != 0 or len(result) < 2: 
-            return None
+def listing_on_device(path):
+    exit_code, output = run_on_device(["ls", "-l", f"\"{path}\"", "|", "grep", "'^-'"])
+    print(output)
+    if exit_code != 0:
+        raise RuntimeError(f"Failed to query directory: {path}")
 
-        cols = result[-1].split()
-        if len(cols) == 0:
-            return None
+    return list(map(lambda line: line.split(maxsplit = 6)[6], output))
 
-        is_file = cols[0][0] == '-'
-        if len(cols) < (6 if is_file else 5): 
-            return None
+def count_children(path):
+    count = len(os.listdir(path))
+    count_full = 0
+    for _, _, fnames in os.walk(path): count_full += len(fnames)
+    return count, count_full
 
-        time_str = " ".join(cols[4:6] if is_file else cols[3:5])
-        return datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M").timestamp()
+def count_children_on_device(path):
+    exit_code, output = run_on_device(["ls", "-l", f"\"{path}\""])
+    if exit_code != 0:
+        raise RuntimeError(f"Failed to query directory: {path}")
+
+    count = len(output)
+
+    exit_code, output = run_on_device(["ls", "-lR", f"\"{path}\"", "|", "grep", "'^-'"])
+    if exit_code != 0:
+        raise RuntimeError(f"Failed to query directory: {path}")
+
+    count_full = len(output)
+
+    return count, count_full
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -83,55 +84,70 @@ if __name__ == "__main__":
     if not os.path.exists(args.source):
         raise ValueError("Provided <source> path does not exist: {args.source}")
 
-    try:
-        shell = DeviceShell()
-    except Exception as e:
-        print(f"{e}")
-        exit(1)
+    result = subprocess.run(["which", "adb"],
+        stdout = subprocess.DEVNULL,
+        stderr = subprocess.DEVNULL)
 
-    if not shell.exists(args.destination):
+    if result.returncode != 0:
+        raise RuntimeError("Unable to find adb in PATH!")
+
+    result = subprocess.run(["adb", "devices"],
+        capture_output = True,
+        text = True)
+
+    if result.returncode != 0 or len(result.stdout.strip().split("\n")) != 2:
+        raise RuntimeError("Couldn't find device!")
+
+    if not exists_on_device(args.destination):
         raise ValueError(f"Provided <destination> path does not exist: {args.destination}")
 
-    args.source = args.source.rstrip("/")
-    args.destination = os.path.join(args.destination, os.path.basename(args.source))
+    path_source = args.source.rstrip("/")
+    path_device = os.path.join(args.destination, os.path.basename(path_source))
 
-    dirs = [(dirname.removeprefix(args.source).lstrip("/"), fnames) 
-        for dirname, _, fnames in os.walk(args.source) if fnames]
+    if not exists_on_device(path_device):
+        result = subprocess.run(["adb", "push", path_source, args.destination], 
+            stdout = sys.stdout, stderr = sys.stderr)
+        if result.returncode != 0:
+            raise RuntimeError("Failed to perform initial sync!")
+        exit(0)
 
-    dirs_to_push = []
-    for entry, fnames in tqdm.tqdm(dirs, desc = "Indexing"):
-        path_src = os.path.join(args.source, entry)
-        path_dst = os.path.join(args.destination, entry)
+    entries_to_skip = set()
+    entries = []
+    for entry_source, children_dirname, children_fname in os.walk(path_source, topdown = True):
+        if len(children_dirname) == 0 and len(children_fname) == 0: continue
 
-        if shell.exists(path_dst):
-            path_dst_mod = shell.getmtime(path_dst)
-            exit_code, fnames_dst = shell.run(f"ls -l \"{path_dst}\"")
-            if exit_code != 0:
-                raise RuntimeError("Unreachable!")
-            if len(fnames) == len(fnames_dst) and path_dst_mod is not None and path_dst_mod >= os.path.getmtime(path_src): continue
-            dirs_to_push.append((entry, True))
+        entry = entry_source.removeprefix(path_source).lstrip("/")
+        if len(entry) == 0: continue
+        if any(entry.startswith(skip) for skip in entries_to_skip): continue
+
+        entry_device = os.path.join(path_device, entry)
+        if not exists_on_device(entry_device):
+            result = subprocess.run(["adb", "push", f"{entry_source}", f"{entry_device}"],
+                stdout = sys.stdout, stderr = sys.stderr)
+            if result.returncode != 0:
+                raise RuntimeError(f"Failed to copy file: {entry}")
+            
+            entries_to_skip.add(entry)
         else:
-            dirs_to_push.append((entry, False))
-
-    dirs_to_push_filtered = []
-    class BreakOuter(Exception): pass
-    for entry_a, exists_a in dirs_to_push:
-        try:
-            for entry_b, exists_b in dirs_to_push:
-                if entry_a == entry_b: continue
-                if pathlib.Path(entry_a).resolve().is_relative_to(pathlib.Path(entry_b).resolve()):
-                    raise BreakOuter;
-            dirs_to_push_filtered.append((entry_a, exists_a))
-        except BreakOuter:
-            pass
-    
-    for entry, exists in tqdm.tqdm(dirs_to_push_filtered, desc = "Patching"):
-        path_src = os.path.join(args.source, entry)
-        path_dst = os.path.join(args.destination, entry)
-        if exists: 
-            path_dst = os.path.dirname(path_dst)
-
-        cmd = subprocess.run(["adb", "push", f"{path_src}", f"{path_dst}"],
-            stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-        if cmd.returncode != 0:
-            raise RuntimeError(f"Failed to copy file: {entry}")
+            count_source, count_full_source = count_children(entry_source)
+            count_device, count_full_device = count_children_on_device(entry_device)
+            if count_device == 0:
+                result = subprocess.run(["adb", "push", f"{entry_source}", f"{os.path.dirname(entry_device)}"],
+                    stdout = sys.stdout, stderr = sys.stderr)
+                if result.returncode != 0:
+                    raise RuntimeError(f"Failed to copy file: {entry}")
+                
+                entries_to_skip.add(entry)
+            elif count_full_source == count_full_device:
+                entries_to_skip.add(entry)
+            else:
+                children_fname_missing = []
+                for f in children_fname:
+                    if exists_on_device(os.path.join(entry_device, f)): continue
+                    children_fname_missing.append(os.path.join(entry_source, f))
+                
+                if len(children_fname_missing) > 0:
+                    result = subprocess.run(["adb", "push", *children_fname_missing, f"{entry_device}"],
+                        stdout = sys.stdout, stderr = sys.stderr)
+                    if result.returncode != 0:
+                        raise RuntimeError(f"Failed to copy file: {entry}")
